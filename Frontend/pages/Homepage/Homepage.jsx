3db87f74410f044af829184ad7cbba2882873349
alexgburnet <alexburnet03@gmail.com>
2024-11-28 18:21:17 +0000

    refactor to new folder, added sidebar component

---
+++ Frontend/pages/Homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "../../components/Sidebar/Sidebar";
+
+function Homepage () {
+
+
+    return (
+        <div className="homepage">
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Homepage;
