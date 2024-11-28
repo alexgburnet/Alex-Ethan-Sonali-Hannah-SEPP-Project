b5ac304fd1cbba38911c588d79158ac5168d1ca5
alexgburnet <alexburnet03@gmail.com>
2024-11-28 17:09:51 +0000

    Added hompage react component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
+import Homepage from '../pages/Homepage.jsx';
+
 import './App.css'
 
 function App() {
@@ -8,7 +10,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<h1>home</h1>} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/product" element={<h1>product</h1>} />
         <Route path="/cart" element={<h1>cart</h1>} />
       </Routes>
