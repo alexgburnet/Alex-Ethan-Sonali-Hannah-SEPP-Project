3c457688ac4c3775f20ce71a8484a9744d7aba0b
alexgburnet <alexburnet03@gmail.com>
2024-11-28 18:18:52 +0000

    Wrapped routes in page container

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import Homepage from '../pages/Homepage.jsx';
+import Homepage from '../pages/Homepage/Homepage.jsx';
 import Navbar from '../Components/Navbar/Navbar.jsx';
 import "./App.css"
 function App() {
@@ -9,11 +9,13 @@ function App() {
   return (
     <div>
       <Navbar/>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/product" element={<h1>product</h1>} />
-        <Route path="/cart" element={<h1>cart</h1>} />
-      </Routes>
+      <div className='page-container'>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/product" element={<h1>product</h1>} />
+          <Route path="/cart" element={<h1>cart</h1>} />
+        </Routes>
+      </div>
     </div>
   )
 }
