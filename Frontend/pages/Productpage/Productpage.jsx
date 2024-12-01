10163605137db912535776a0c1943b6b1945764a
alexgburnet <alexburnet03@gmail.com>
2024-12-01 19:29:14 +0000

    Added image gallery with placeholder images

--- Frontend/pages/Productpage/Productpage.jsx
+++ Frontend/pages/Productpage/Productpage.jsx
@@ -1,14 +1,36 @@
+import ImageGallery from 'react-image-gallery';
+
 import './Productpage.css';
+import 'react-image-gallery/styles/css/image-gallery.css';
 
 function ProductPage() {
 
+    const images = [
+        {
+          original: "https://picsum.photos/id/1018/1000/600/",
+          thumbnail: "https://picsum.photos/id/1018/250/150/",
+        },
+        {
+          original: "https://picsum.photos/id/1015/1000/600/",
+          thumbnail: "https://picsum.photos/id/1015/250/150/",
+        },
+        {
+          original: "https://picsum.photos/id/1019/1000/600/",
+          thumbnail: "https://picsum.photos/id/1019/250/150/",
+        },
+      ];
+      
     
   return (
     <div className="product-page">
         <div className='product-container'>
 
             <div className='image-container'>
-                <p>image container</p>
+                <ImageGallery 
+                    items={images}
+                    showPlayButton={false}
+                    showFullscreenButton={false}
+                />
             </div>
 
             <div className='product-info-container'>
