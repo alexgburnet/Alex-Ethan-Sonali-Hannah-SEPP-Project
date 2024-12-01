46ac996f41af605bf20d57c469cd41d23808d4d4
alexgburnet <alexburnet03@gmail.com>
2024-12-01 16:59:50 +0000

    Added useStates for search queries to allow search bar to communicate with Item Container

--- Frontend/pages/Homepage/Homepage.jsx
+++ Frontend/pages/Homepage/Homepage.jsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
+
 import Sidebar from "../../Components/Sidebar/Sidebar.jsx";
-import Searchbar from "../../components/Searchbar/Searchbar.jsx";
+import Searchbar from "../../Components/Searchbar/Searchbar.jsx";
 import ItemContainer from "../../Components/ItemContainer/ItemContainer.jsx";
 
 import './Homepage.css'
 
 function Homepage () {
 
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchResult, setSearchResult] = useState('');
+
+    const handleSearch = (query) => {
+        setSearchQuery(query);
+    }
 
     return (
         <div className="homepage">
             <Sidebar />
             <div className="items-search">
-                <Searchbar />
-                <ItemContainer />
+                <Searchbar setSearchQuery={setSearchQuery} onSearch={handleSearch}/>
+                <ItemContainer searchQuery={searchResult}/>
             </div>
         </div>
     )
