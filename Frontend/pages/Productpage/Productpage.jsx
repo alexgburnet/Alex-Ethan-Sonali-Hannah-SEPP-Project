ecd4c0fd0494738e1a67a7d89caec4b259130ada
Ethan <dusty3692@gmail.com>
2024-12-02 16:43:49 +0000

    Modified props to use either passed props from location or default props

--- Frontend/pages/Productpage/Productpage.jsx
+++ Frontend/pages/Productpage/Productpage.jsx
@@ -1,61 +1,62 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import ImageGallery from 'react-image-gallery';
 
 import './Productpage.css';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
-function ProductPage(props) {
+function ProductPage() {
+    const location = useLocation();
+    const { imgSource, itemName, itemDescription, price } = location.state || {};
 
     const images = [
         {
-          original: props.imgSource,
+            original: imgSource || ProductPage.defaultProps.imgSource,
         }
-      ];
-      
-    
-  return (
-    <div className="product-page">
-        <div className='product-container'>
+    ];
 
-            <div className='image-container'>
-                <ImageGallery 
-                    items={images}
-                    showPlayButton={false}
-                    showFullscreenButton={false}
-                />
-            </div>
-
-            <div className='product-info-container'>
-                <h1>{props.itemName}</h1>
-                <p>${props.price}</p>
-
-                <hr></hr>
-
-                <div className='quantity-container'>
-                    <h3>Quantity</h3>
-                    <input type='number' min='1' max='10' defaultValue='1'></input>
+    return (
+        <div className="product-page">
+            <div className='product-container'>
+                <div className='image-container'>
+                    <ImageGallery 
+                        items={images}
+                        showPlayButton={false}
+                        showFullscreenButton={false}
+                    />
                 </div>
 
-                <hr></hr>
+                <div className='product-info-container'>
+                    <h1>{itemName || ProductPage.defaultProps.itemName}</h1>
+                    <p>${price || ProductPage.defaultProps.price}</p>
 
-                <button className='add-to-cart'>Add to cart</button>
+                    <hr></hr>
 
-                <p>{props.itemDescription}</p>
+                    <div className='quantity-container'>
+                        <h3>Quantity</h3>
+                        <input type='number' min='1' max='10' defaultValue='1'></input>
+                    </div>
 
+                    <hr></hr>
 
+                    <button className='add-to-cart'>Add to cart</button>
+
+                    <p>{itemDescription || ProductPage.defaultProps.itemDescription}</p>
+                </div>
             </div>
 
+            <div className='similar-products-container'>
+                <p>similar products</p>
+            </div>
         </div>
+    );
+}
 
-        <div className='similar-products-container'>
-            <p>similar products</p>
-        </div>
-    </div>
-  );
-}
 ProductPage.defaultProps = {
-  imgSource: "https://via.placeholder.com/150",
-  itemName: "Item Name",
-  itemDescription: "Item Description",
-  price: "0.00"
+    imgSource: "https://via.placeholder.com/150",
+    itemName: "Item Name",
+    itemDescription: "Item Description",
+    price: "0.00"
 }
+
 export default ProductPage;
