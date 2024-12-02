5a7383e38b49791ceb62141fc5837f4066d5e441
Ethan <dusty3692@gmail.com>
2024-12-02 15:49:27 +0000

    Created a temporary array filled with cards for testing

--- Frontend/Components/ItemContainer/ItemContainer.jsx
+++ Frontend/Components/ItemContainer/ItemContainer.jsx
@@ -1,9 +1,20 @@
 import './ItemContainer.css';
+import ItemCard from '../ItemCard/ItemCard';
 
 function ItemContainer({ searchQuery }) {
+  const cardsdisplayed = [];
+  //temporary for loop to generate a lot of cards for testing display
+  for (let i = 0; i < 50; i++) {
+    cardsdisplayed.push(<ItemCard />);
+  }
   return (
     <div className="item-container">
-      <p>{ searchQuery ? searchQuery : "Type something and search to display search query" }</p>
+      <div className="search-result">
+        <p>{ searchQuery ? searchQuery : "Type something and search to display search query" }</p>
+      </div>
+      <div className="item-cards-grid">
+        {cardsdisplayed}
+      </div>
     </div>
   );
 }
