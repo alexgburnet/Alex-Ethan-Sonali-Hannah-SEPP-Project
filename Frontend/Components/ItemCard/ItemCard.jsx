099c2c774f1ce01d3454e2e777008f8b7989769f
Ethan <dusty3692@gmail.com>
2024-12-02 16:42:36 +0000

    Created Link tag to pass along product props to ProductPage

--- Frontend/Components/ItemCard/ItemCard.jsx
+++ Frontend/Components/ItemCard/ItemCard.jsx
@@ -1,19 +1,31 @@
 import './ItemCard.css';
+import { Link } from 'react-router-dom';
 
 function ItemCard(props) {
     return(
-        <div className="ItemCard">
-            <div className="ItemCardImage">
-                <img src={props.imgSource} alt="Item" />
+        <Link to={{
+            pathname: "/product",
+            state: {
+                imgSource: props.imgSource,
+                itemName: props.itemName,
+                itemDescription: props.itemDescription,
+                price: props.price
+            }
+        }} className="ItemCardLink">
+            <div className="ItemCard">
+                <div className="ItemCardImage">
+                    <img src={props.imgSource} alt="Item" />
+                </div>
+                <div className="ItemCardDetails">
+                    <h3>{props.itemName}</h3>
+                    <p>{props.itemDescription}</p>
+                    <p>Price: ${props.price}</p>
+                </div>
             </div>
-            <div className="ItemCardDetails">
-                <h3>{props.itemName}</h3>
-                <p>{props.itemDescription}</p>
-                <p>Price: ${props.price}</p>
-            </div>
-        </div>
+        </Link>
     );
 }
+
 ItemCard.defaultProps = {
     imgSource: "https://via.placeholder.com/150",
     itemName: "Item Name",
