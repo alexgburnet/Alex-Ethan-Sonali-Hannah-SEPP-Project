1b0310fe18dfa9ecadd67ebd9d4c9c8a6e1a4bff
alexgburnet <alexburnet03@gmail.com>
2024-12-06 15:15:53 +0000

    Added sqlalchemy for database queries

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# Change the URL here for our databse
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/your_database'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    # EXAMPLE of a SELECT query to retrieve data from a table
+    result = db.engine.execute("SELECT * FROM your_table")
+    rows = [dict(row) for row in result]
+    return {'data': rows}
 
 
 if __name__ == "__main__":
