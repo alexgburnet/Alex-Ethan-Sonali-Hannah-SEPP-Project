0b3b7afa63726d24fa2d633d4d6a75e2cf301e22
alexgburnet <alexburnet03@gmail.com>
2024-12-06 15:03:31 +0000

    Added back-end requirement install/start

--- run.sh
+++ run.sh
@@ -2,4 +2,20 @@
 
 cd Frontend
 npm install
-npm run dev
+npm run dev &
+
+cd ../Backend
+
+
+if command -v python3 &> /dev/null
+then
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+    python app.py
+else
+    python -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+    python app.py
+fi
