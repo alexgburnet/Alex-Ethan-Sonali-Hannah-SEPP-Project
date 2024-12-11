b9af647e19000d9049468b7e1c2a4ae4d10ca442
alexgburnet <alexburnet03@gmail.com>
2024-12-11 21:29:54 +0000

    added log-out button that clear cookies

--- Frontend/Components/Navbar/Navbar.jsx
+++ Frontend/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Cookies from 'js-cookie';
 import { useState, useEffect } from "react";
 
 import "./Navbar.css";
@@ -27,9 +27,29 @@ function Navbar () {
     };
   }, []);
 
+  const handleLogout = () => {
+    Cookies.remove('user_id');
+    Cookies.remove('order_id');
+    window.location.href = '/';
+  }
+
 
   return (
     <header>
+      <div className="LogoutIcon" onClick={handleLogout} title="Logout" style={{ cursor: 'pointer' }}>
+        <svg xmlns="http://www.w3.org/2000/svg" 
+             viewBox="0 0 24 24" 
+             fill="none" 
+             stroke="#000000" 
+             strokeWidth="3" 
+             strokeLinecap="round" 
+             strokeLinejoin="round" 
+             className="feather feather-log-out">
+          <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+          <polyline points="16 17 21 12 16 7"></polyline>
+          <line x1="21" y1="12" x2="9" y2="12"></line>
+        </svg>
+      </div>
       <div className="SiteNameHeader">
         <Link to="/" className="link">
           <h1>{title}</h1>
